69981a059dec317088bf9c022b90e7d8a39a2e4f
Daniel <danielusuario21@gmail.com>
2023-09-28 09:16:08 -0300

    Criada a aplicação e iniciado o Controller Usuario

---
+++ ApiLogin/Controllers/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiLogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Usuario : ControllerBase
+    {
+        [Required(ErrorMessage = "O campo Email é obrigatório!")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string Senha { get; set; }
+
+        private List<Usuario> _usuarios;
+        [HttpPost("Adicionar")]
+
+
+
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
---
+++ ApiLogin/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
