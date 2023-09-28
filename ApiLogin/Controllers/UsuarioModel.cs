2e8e72a1f0f4356df16321eadfd676bec8a2d8cc
Daniel <danielusuario21@gmail.com>
2023-09-28 11:14:06 -0300

    Adicionado Model de Usuario e feito a separação entre Controller e Model

---
+++ ApiLogin/Controllers/UsuarioModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using ApiLogin.Models;
+
+namespace ApiLogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Usuario : ControllerBase
+    {
+
+        private List<UsuarioModel> _usuarios = new List<UsuarioModel>();
+        [HttpPost("Adicionar")]
+
+        public ActionResult AdicionarUsuario([FromBody] UsuarioModel novoUsuario)
+        {
+            if(novoUsuario == null)
+            {
+                return BadRequest("Dados Invalidos!!");
+            }
+
+            _usuarios.Add(novoUsuario);
+
+            return CreatedAtAction(nameof(Get), new { email = novoUsuario.Email }, novoUsuario);
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
---
+++ ApiLogin/Models/UsuarioModel.cs
@@ -0,0 +1,17 @@
+﻿using ApiLogin.Controllers;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiLogin.Models
+{
+    public class UsuarioModel
+    {
+        [Required(ErrorMessage = "O campo Email é obrigatório!")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string Senha { get; set; }
+        
+    }
+}
